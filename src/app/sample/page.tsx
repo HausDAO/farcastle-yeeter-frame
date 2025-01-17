c254d03ea1aafc66f9d7075661d23f13deaa74b8
earth2travis <travis.mccutcheon@gmail.com>
2025-01-17 13:25:47 -0600

    Resolve TypeScript errors at the sample route

--- src/app/sample/page.tsx
+++ src/app/sample/page.tsx
@@ -3,11 +3,30 @@
 import React from 'react';
 
 import { SampleForm } from '@/components/forms/SampleForm';
+import { BaalContractBase } from '@/lib/tx-prepper/tx-prepper';
 
 export default function CharacterCreatorPage() {
   return (
     <div className="w-full h-full space-y-4 pt-16 grow">
-      <SampleForm />
+      <SampleForm
+        formConfig={{
+          id: 'sample',
+          tx: {
+            id: 'sample',
+            method: 'submitProposal',
+            contract: {
+              ...BaalContractBase,
+              type: 'static',
+              targetAddress: '0x...',
+            },
+            args: [],
+          },
+        }}
+        confirmed={false}
+        loading={false}
+        invalidConnection={false}
+        handleSubmit={async () => {}}
+      />
     </div>
   );
 }
