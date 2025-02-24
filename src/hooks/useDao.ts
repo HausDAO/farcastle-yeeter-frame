c433e486f0ab54f0505e2766eb0bdce90007aa31
skuhlmann <samkuhlmann@gmail.com>
2025-02-24 12:20:19 -0700

    add some logs

--- src/hooks/useDao.ts
+++ src/hooks/useDao.ts
@@ -33,6 +33,8 @@ export const useDao = ({
     );
   }
 
+  console.log("useDao chainid daoid", chainid, daoid);
+
   const { data, ...rest } = useQuery<{ dao: DaoItem }>({
     queryKey: [`get-dao-${chainid}-${daoid}`],
     enabled: Boolean(chainid && daoid),
