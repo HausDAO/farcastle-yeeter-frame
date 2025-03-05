0f7846b70e44c51609091b36a9dc5f38db7a33d7
skuhlmann <samkuhlmann@gmail.com>
2025-03-05 09:39:50 -0700

    try with more dynamic meta

--- src/app/dao/[chainid]/[daoid]/page.tsx
+++ src/app/dao/[chainid]/[daoid]/page.tsx
@@ -1,47 +1,48 @@
 import { Card } from "@/components/ui/card";
-// import { Metadata } from "next";
+import { Metadata } from "next";
 import DaoHome from "./dao-home";
 
-// export const runtime = "edge"; // Required for Cloudflare Pages
-// const appUrl = process.env.NEXT_PUBLIC_URL;
+type Props = {
+  params: Promise<{ chainid: string; daoid: string }>;
+};
 
-// export const revalidate = 300;
+export async function generateMetadata({ params }: Props): Promise<Metadata> {
+  console.log("meta params", params);
 
-// type Props = {
-//   params: Promise<{ chainid: string; daoid: string }>;
-// };
+  const { chainid, daoid } = await params;
 
-// export async function generateMetadata({ params }: Props): Promise<Metadata> {
-//   const { chainid, daoid } = await params;
+  const frame = {
+    version: "next",
+    imageUrl: `https://proposals.farcastle.net/image.png`,
+    button: {
+      title: "Make Proposal (DAO)",
+      action: {
+        type: "launch_frame",
+        name: "Proposals (DAO)",
+        url: `https://proposals.farcastle.net/dao/${chainid}/${daoid}`,
+        iconImageUrl: `https://proposals.farcastle.net/icon.png`,
+        splashImageUrl: `https://proposals.farcastle.net/splash.png`,
+        splashBackgroundColor: "#341A34",
+      },
+    },
+  };
+  return {
+    metadataBase: new URL("https://proposals.farcastle.net"),
+    title: "Proposals (DAO)",
+    openGraph: {
+      title: "Farcastle Proposals (DAO)",
+      description: "the actions of organizations",
+      images: `https://proposals.farcastle.net/image.png`,
+    },
+    other: {
+      "fc:frame": JSON.stringify(frame),
+    },
+  };
+}
 
-//   const frame = {
-//     version: "next",
-//     imageUrl: `${appUrl}/opengraph-image`,
-//     button: {
-//       title: "Launch",
-//       action: {
-//         type: "launch_frame",
-//         name: "#3 Farcastle DAO Proposals",
-//         url: `${appUrl}/dao/${chainid}/${daoid}`,
-//         splashImageUrl: `${appUrl}/splash.png`,
-//         splashBackgroundColor: "#17151F",
-//       },
-//     },
-//   };
+export default function Page({ params }: Props) {
+  console.log("page params", params);
 
-//   return {
-//     title: "#1 Farcastle DAO Proposals",
-//     openGraph: {
-//       title: "#2 Farcastle DAO Proposals",
-//       description: "Farcastle DAO Proposals",
-//     },
-//     other: {
-//       "fc:frame": JSON.stringify(frame),
-//     },
-//   };
-// }
-
-export default function Page() {
   return (
     <div className="w-full h-full pb-4 px-4">
       <Card className="flex flex-col items-center pt-4 pb-8 rounded-none">
