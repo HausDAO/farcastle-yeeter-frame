2573b191744c1249248333107f6e5d9300c811b9
skuhlmann <samkuhlmann@gmail.com>
2025-03-05 09:19:34 -0700

    try not extra meta

--- src/app/dao/[chainid]/[daoid]/page.tsx
+++ src/app/dao/[chainid]/[daoid]/page.tsx
@@ -1,45 +1,45 @@
 import { Card } from "@/components/ui/card";
-import { Metadata } from "next";
+// import { Metadata } from "next";
 import DaoHome from "./dao-home";
 
-export const runtime = "edge"; // Required for Cloudflare Pages
-const appUrl = process.env.NEXT_PUBLIC_URL;
+// export const runtime = "edge"; // Required for Cloudflare Pages
+// const appUrl = process.env.NEXT_PUBLIC_URL;
 
-export const revalidate = 300;
+// export const revalidate = 300;
 
-type Props = {
-  params: Promise<{ chainid: string; daoid: string }>;
-};
+// type Props = {
+//   params: Promise<{ chainid: string; daoid: string }>;
+// };
 
-export async function generateMetadata({ params }: Props): Promise<Metadata> {
-  const { chainid, daoid } = await params;
+// export async function generateMetadata({ params }: Props): Promise<Metadata> {
+//   const { chainid, daoid } = await params;
 
-  const frame = {
-    version: "next",
-    imageUrl: `${appUrl}/opengraph-image`,
-    button: {
-      title: "Launch",
-      action: {
-        type: "launch_frame",
-        name: "#3 Farcastle DAO Proposals",
-        url: `${appUrl}/dao/${chainid}/${daoid}`,
-        splashImageUrl: `${appUrl}/splash.png`,
-        splashBackgroundColor: "#17151F",
-      },
-    },
-  };
+//   const frame = {
+//     version: "next",
+//     imageUrl: `${appUrl}/opengraph-image`,
+//     button: {
+//       title: "Launch",
+//       action: {
+//         type: "launch_frame",
+//         name: "#3 Farcastle DAO Proposals",
+//         url: `${appUrl}/dao/${chainid}/${daoid}`,
+//         splashImageUrl: `${appUrl}/splash.png`,
+//         splashBackgroundColor: "#17151F",
+//       },
+//     },
+//   };
 
-  return {
-    title: "#1 Farcastle DAO Proposals",
-    openGraph: {
-      title: "#2 Farcastle DAO Proposals",
-      description: "Farcastle DAO Proposals",
-    },
-    other: {
-      "fc:frame": JSON.stringify(frame),
-    },
-  };
-}
+//   return {
+//     title: "#1 Farcastle DAO Proposals",
+//     openGraph: {
+//       title: "#2 Farcastle DAO Proposals",
+//       description: "Farcastle DAO Proposals",
+//     },
+//     other: {
+//       "fc:frame": JSON.stringify(frame),
+//     },
+//   };
+// }
 
 export default function Page() {
   return (
