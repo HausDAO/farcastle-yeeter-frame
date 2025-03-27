24dc55f875a8d421abaf1dcb275c6c1bbeffa0b0
skuhlmann <samkuhlmann@gmail.com>
2025-03-27 12:55:51 -0600

    homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -27,23 +27,20 @@ export default function Page() {
 
   const { connector } = useFrameSDK();
   const { connect } = useConnect();
-  const { isConnected, chain } = useAccount();
-
-  const chainNames = {
-    // Ethereum: "Ethereal",
-    "OP Mainnet": "Optimistic",
-    Base: "Based",
-    Gnosis: "Gnostic",
-    "Arbitrum One": "Arbitral",
-    // Polygon: "Polymorphic",
-    Sepolia: "Sepolic",
-  };
+  const { isConnected } = useAccount();
 
   if (!mounted) return null;
 
   return (
     <div className="w-full h-full pb-4 px-4">
       <Card className="flex flex-col items-center px-4 pt-4 pb-8 rounded-none">
+        <div className="text-center mb-3">
+          <p className="mb-3">
+            Crowdfunding reimagined with web3 transparency and community
+            protection.
+          </p>
+          <p>On-chain and In-frame.</p>
+        </div>
         {!isConnected && (
           <>
             <div className="text-muted font-display text-3xl uppercase mb-4">
@@ -64,7 +61,7 @@ export default function Page() {
                 onClick={() => connect({ connector: connector })}
                 className="w-full"
               >
-                Reveal Realms
+                Reveal Raises
               </Button>
             </div>
           </>
@@ -75,13 +72,13 @@ export default function Page() {
             <div className="flex flex-col w-full gap-3">
               <div className="mt-4 w-full px-4">
                 <Link href={`/explore`} className="w-full">
-                  <Button className="w-full">Explore Fundraises</Button>
+                  <Button className="w-full">Explore</Button>
                 </Link>
               </div>
 
               <div className="mt-2 w-full px-4">
                 <Link href={`/launch`} className="w-full">
-                  <Button className="w-full">Create a Fundraiser</Button>
+                  <Button className="w-full">Launch</Button>
                 </Link>
               </div>
             </div>
