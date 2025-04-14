50e225f3e9e0115f66cac46e8918b99ea7f0e04f
e2t <accounts@earth2travis.com>
2025-04-13 21:35:06 -0500

    style: update text and tweak styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -25,8 +25,8 @@ export default function Page() {
 
   return (
     <div className="w-full h-full pb-4 px-4">
-      <Card className="flex flex-col items-center px-4 pt-2 pb-4 rounded-none">
-        <div className="text-muted font-display text-3xl uppercase mb-4">
+      <Card className="flex flex-col items-center px-4 pt-4 pb-4 rounded-none">
+        <div className="text-primary font-display text-3xl uppercase mb-4">
               Crypto Crowdfunding
         </div>
         {!isConnected && (
@@ -41,7 +41,7 @@ export default function Page() {
                 className="object-contain"
               />
             </div>
-            <div className="mt-4 w-full px-4 mb-4">
+            <div className="mt-4 w-full px-4 mb-0">
               <Button
                 onClick={() => connect({ connector: connector })}
                 className="w-full"
@@ -59,8 +59,16 @@ export default function Page() {
 
         {isConnected && (
           <>
-            <p className="text-muted-foreground text-base px-4">Brave builders forge new tools while supporters join their quest.</p>
-            <div className="flex flex-col w-full gap-3">
+            <p className="text-muted-foreground text-base px-4 mb-4 italic">Ignite the flames of collective action in the realm of Web3.</p>
+            <div className="text-primary font-display text-2xl uppercase mb-2">
+          For Builders of the New Order
+        </div>
+            <p className="text-muted-foreground text-base px-4 mb-4">Fundraiser lets creators raise funds with trust and transparency. Contributors pledge coins and retain the right to exit.</p>
+            <div className="text-primary font-display text-2xl uppercase mb-2">
+            Your Path Awaits
+        </div>
+            <p className="text-muted-foreground text-base px-4 mb-4">Explore campaigns and identify opportunities to contribute. Or launch a campaign and raise funds for your quest.</p>
+            <div className="flex flex-col w-full gap-2">
               <div className="mt-4 w-full px-4">
                 <Link href={`/explore`} className="w-full">
                   <Button className="w-full">Explore</Button>
