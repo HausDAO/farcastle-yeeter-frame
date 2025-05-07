d223055b093fa9d6b99743fe54f33040815d572e
e2t <accounts@earth2travis.com>
2025-05-06 23:51:29 -0500

    feat: add explore | launch navigation

--- src/components/ui/header.tsx
+++ src/components/ui/header.tsx
@@ -9,18 +9,31 @@ function Header() {
   const router = useRouter();
   const pathname = usePathname();
 
-  const onTitleClick = React.useCallback(() => {
-    router.push("/");
+  const onExploreClick = React.useCallback(() => {
+    router.push("/explore");
+  }, [router]);
+
+  const onLaunchClick = React.useCallback(() => {
+    router.push("/launch");
   }, [router]);
 
   return (
     <div className="w-full flex flex-row items-end justify-between p-4">
       <NetworkSwitcher />
-      <div
-        className={`text-3xl font-fraktur pt-[2px] leading-[30px] h-[30px] text-primary ${pathname !== "/" ? "hover:cursor-pointer" : ""}`}
-        onClick={onTitleClick}
-      >
-        fundraiser
+      <div className="text-3xl font-fraktur pt-[2px] leading-[30px] h-[30px] text-primary flex gap-2">
+        <span
+          className="hover:cursor-pointer"
+          onClick={onExploreClick}
+        >
+          explore
+        </span>
+        <span className="mx-2 h-[28px] w-px bg-muted inline-block align-middle"></span>
+        <span
+          className="hover:cursor-pointer"
+          onClick={onLaunchClick}
+        >
+          launch
+        </span>
       </div>
       <ConnectWallet />
     </div>
