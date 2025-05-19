27eeb4c605b6496c53d15ed766c4dcf6de7172dc
skuhlmann <samkuhlmann@gmail.com>
2025-05-19 08:53:14 -0600

    tries to clean up some tags

--- src/app/yeeter/[chainid]/[yeeterid]/rewards/page.tsx
+++ src/app/yeeter/[chainid]/[yeeterid]/rewards/page.tsx
@@ -19,7 +19,6 @@ export async function generateMetadata({ params }: Props): Promise<Metadata> {
 
   const frame = {
     version: "next",
-    // imageUrl: `${baseUrl}/yeeter/${chainid}/${yeeterid}/yeet/${amount}/opengraph-image`,
     imageUrl: imageUrl.toString(),
     button: {
       title: "Contribute",
@@ -27,29 +26,22 @@ export async function generateMetadata({ params }: Props): Promise<Metadata> {
         type: "launch_frame",
         name: "Fundraiser",
         url: `${baseUrl}/yeeter/${chainid}/${yeeterid}`,
-        iconImageUrl: `${baseUrl}/icon.png`,
         splashImageUrl: `${baseUrl}/splash.png`,
         splashBackgroundColor: "#341A34",
       },
     },
-    input: {
-      text: "Enter your message (optional)",
-    },
-    postUrl: `${baseUrl}/api/frame`,
   };
   return {
     title: "Fundraiser",
     openGraph: {
       title: "Farcastle Fundraiser",
       description: "Enrich the realm",
-      // images: [`${baseUrl}/yeeter/${chainid}/${yeeterid}/opengraph-image`],
       images: [{ url: imageUrl.toString() }],
     },
     other: {
       "fc:frame": JSON.stringify(frame),
       "fc:frame:image": `${imageUrl.toString()}`,
       "fc:frame:button:1": "Contribute",
-      "fc:frame:post_url": `${baseUrl}/api/frame`,
     },
   };
 }
